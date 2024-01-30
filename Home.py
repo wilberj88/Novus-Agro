32e175c3a9612cecc2660f72bb2febe133f41b6a
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-30 23:41:02 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,17 @@ import streamlit as st
 
 st.set_page_config(
     layout = 'wide',
-    page_title = 'Agro Twin'
+    page_title = 'Agro Twin',
+    page_icon = 👤,
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.agrotwin.com/help',
+        'Report a bug': "https://www.agrotwin.com/bug",
+        'About': "# Tu Gemelo Digital en la Nube de tu proyecto *Agro*!"
+    }
 )
 
 st.title("Agro Twin")
+st.header("Configuración")
+st.header("Diagnóstico")
+st.header("Gemelo Digital")
