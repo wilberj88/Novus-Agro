39699a33672566037e22b008d7e7464257a89953
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-31 00:21:21 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ import altair as alt
 st.set_page_config(
     layout = 'wide',
     page_title = 'Agro Twin',
-    page_icon=👤,
+    page_icon=👨‍🌾,
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://www.agrotwin.com/help',
