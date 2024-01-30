507fe739ce217ad995edc80db9bea019f07237ba
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-31 00:07:34 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,5 +15,29 @@ st.set_page_config(
 
 st.title("Agro Twin")
 st.header("Configuración")
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    cultivo = st.radio(
+    "¿Cuál es tu cultivo?",
+    ["***Pistacho***", "***Cacao***"],
+    captions = ["Kerman", "CNCH-13"])
+
+with col2:
+    area = st.slider('Cuántas hectáreas cultivadas?', 0, 100, 5)
+    
+with col3:
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
+    ubicacion = st.selectbox(
+        "En cuál comunidad?",
+        ("Toledo", "Ciudad Real", "Albacete"),
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+    )
+    
+with col4:
+    st.button("Calcular Diagnóstico", type="primary")
+    
 st.header("Diagnóstico")
 st.header("Gemelo Digital")
