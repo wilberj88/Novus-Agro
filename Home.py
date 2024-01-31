f6b64b250edd3f15f44ed53a47509fdeccdcbff9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-31 01:32:43 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -344,79 +344,13 @@ if a:
     st.area_chart(chart_data)
 
     
-    st.header("Plan de Adaptación por zonas")
-    chart_data = pd.DataFrame(
-    np.random.randn(50, 3),
-    columns=["emisiones", "flora", "fauna"])
-    st.bar_chart(chart_data)
     
-    def render_basic_radar():
-        option = {
-                "title": {"text": "Transición energética"},
-                "legend": {"data": ["Consumo Actual", "Consumo Óptimo"]},
-                "radar": {
-                    "indicator": [
-                        {"name": "Agua", "max": 6500},
-                        {"name": "Carbón", "max": 16000},
-                        {"name": "Viento", "max": 30000},
-                        {"name": "Sol", "max": 38000},
-                        {"name": "Petróleo", "max": 52000},
-                        {"name": "Gas", "max": 25000},
-                    ]
-                },
-                "series": [
-                    {
-                        "name": "Consumo Actual Vs Óptimo",
-                        "type": "radar",
-                        "data": [
-                            {
-                                "value": [2000, 10000, 20000, 3500, 15000, 11800],
-                                "name": "Consumo Actual",
-                            },
-                            {
-                                "value": [3500, 15000, 25000, 10800, 22000, 20000],
-                                "name": "Consumo Óptimo",
-                            },
-                        ],
-                    }
-                ],
-            }
-        st_echarts(option, height="500px")
-    render_basic_radar()
-    # Data src:  https://www.kaggle.com/manohar676/hotel-reviews-segmentation-recommended-system
-    # Credit to: Manohar Reddy
-    df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Plotly_Graphs/Radar-chart/ExistingHotels_CustomerVisitsdata-1554810038262.csv")
-    df = df[df['Hotelid'].isin(['hotel_101','hotel_102','hotel_103'])]
-    print(df.iloc[:20, :8])
     
-    df = df.groupby('Hotelid')[['Cleanliness_rating', 'Service_rating', 'Value_rating',
-                                    'Rooms_rating','Checkin_rating',
-                                    'Businessservice_rating']].mean().reset_index()
-    print(df)
-    
-    # Convert from wide data to long data to plot radar chart
-    df = pd.melt(df, id_vars=['Hotelid'], var_name='category', value_name='rating',
-                     value_vars=['Cleanliness_rating', 'Service_rating', 'Value_rating',
-                                 'Rooms_rating','Checkin_rating','Businessservice_rating'],
-        )
-    print(df)
-    
-    # radar chart Plotly examples - https://plotly.com/python/radar-chart/
-    # radar chart Plotly docs = https://plotly.com/python-api-reference/generated/plotly.express.line_polar.html#plotly.express.line_polar
-    fig = px.line_polar(df, r='rating', theta='category', color='Hotelid', line_close=True,
-                                    line_shape='linear',  # or spline
-                            hover_name='Hotelid',
-                            hover_data={'Hotelid':False},
-                            markers=True,
-                            # labels={'rating':'stars'},
-                            # text='Hotelid',
-                            # range_r=[0,10],
-                            direction='clockwise',  # or counterclockwise
-                            start_angle=45
-                            )
-        # fig.update_traces(fill='toself')
-    fig.show()
-    
+   
     st.header("Gemelo Digital")
-
+    st.subheader("Cronograma Mensual de Tareas del Cuaderno de Campo 📓")
+    chart_data = pd.DataFrame(
+    np.random.randn(50, 5),
+    columns=["Clima", "Riego", "Cámaras", "Sensores", "Jardinería"])
+    st.bar_chart(chart_data)
 
